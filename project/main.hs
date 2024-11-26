b2c0c820c876971dcac0224ee1b7c889ffb9569a
ris8z <giuseppe.esposito2@mail.dcu.ie>
2024-11-26 21:29:59 +0000

    end of project 2-3 tree ca haskell

--- project/main.hs
+++ project/main.hs
@@ -1,5 +1,7 @@
 import Data.List (intercalate)
 
+
+
 data Tree t =
     Empty
   | Root1 t (Tree t) (Tree t)
@@ -8,19 +10,19 @@ data Tree t =
 
 
 --add(X, T) returns the 2-3 Tree from adding X to the 2-3 Tree T.
-add :: (Eq a, Ord a, Show a) => a -> Tree a -> Tree a
-add a Empty = Root1 a Empty Empty --base case n1 
-add a (Root1 value _ _)  --base case n2
+add :: (Ord a) => a -> Tree a -> Tree a
+add a Empty = Root1 a Empty Empty 
+add a (Root1 value _ _)
   | a > value = Root2 value a Empty Empty Empty
   | otherwise = Root2 a value Empty Empty Empty
-add a (Root2 x1 x2 left mid right) --rec call
+add a (Root2 x1 x2 left mid right)
   | a <= x1           = Root2 x1 x2 (add a left) mid right
   | x1 < a && a <= x2 = Root2 x1 x2 left (add a mid) right
   | otherwise         = Root2 x1 x2 left mid (add a right)
 
 
 -- member(X, T) returns true if X is in the 2-3 Tree T.
-member :: (Eq a, Ord a, Show a) => a -> Tree a -> Bool
+member :: (Eq a, Ord a) => a -> Tree a -> Bool
 member a Empty = False
 member a (Root1 value _ _)
   | a == value = True
@@ -34,10 +36,11 @@ member a (Root2 x1 x2 left mid right)
 
 
 -- height(T) returns the height of T.
-height :: (Eq a, Ord a, Show a) => Tree a -> Int
+height :: Tree a -> Int
 height Empty = 0
 height (Root1 {}) = 1
-height (Root2 _ _ left mid right) = 1 + max (height left) (max (height mid) (height right))
+height (Root2 _ _ left mid right) = 
+  1 + max (height left) (max (height mid) (height right))
 
 
 toList :: (Show a) => Tree a -> Int -> Int -> [(String, Int, Int)]
@@ -76,26 +79,27 @@ sort f (x:xs) =
     in
       small ++ [x] ++ big
 
+
 prettyPrintHelper :: (Show a) => Tree a -> String
 prettyPrintHelper tree =
   let
-    nodeList = sort isSmaller (toList tree 0 0) 
-    maxLevel = maximum [lvl | (_, lvl, _) <- nodeList] 
-    width = 3 ^ maxLevel * 3
+    allNodes = sort isSmaller (toList tree 0 0) 
+    maxLevel = maximum [lvl | (_, lvl, _) <- allNodes] 
+    maxWidth = 3 ^ maxLevel * 3
     formatLevel :: Int -> String
     formatLevel lvl = 
       let
-        nodesAtLevel = [(val, pos) | (val, l, pos) <- nodeList, l == lvl]
-        line = replicate width ' '  
+        nodesAtLevel = [(val, pos) | (val, l, pos) <- allNodes, l == lvl]
+        line = replicate maxWidth ' '  
         placeNodes :: [(String,Int)] -> String -> String
         placeNodes [] line = line
-        placeNodes ((val, pos):ns) line =
+        placeNodes ((val, pos):tail) line =
           let
-            spacing = width `div` (3 ^ lvl) 
-            idx = pos * spacing + spacing `div` 2
-            newLine = take idx line ++ val ++ drop (idx + length val) line -- here
+            space = maxWidth `div` (3 ^ lvl) 
+            idx = pos * space + space `div` 2
+            newLine = take idx line ++ val ++ drop (idx + length val) line
           in
-            placeNodes ns newLine
+            placeNodes tail newLine
       in 
         placeNodes nodesAtLevel line
   in
@@ -107,4 +111,5 @@ prettyPrint :: (Show a) => Tree a -> IO ()
 prettyPrint a = putStrLn (prettyPrintHelper a)
 
 
+--mytree = Root2 3 10 Empty (Root1 6 Empty Empty) (Root2 15 20 Empty (Root1 18 Empty Empty) Empty)
 mytree = add 18 (add 20 (add 15 (add 6 (add 10 (add 3 Empty)))))
